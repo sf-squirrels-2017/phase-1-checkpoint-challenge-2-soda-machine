1a9fd554b3e8497e76b9ef9e81256501f85f9b40
kimberly embry <kimberlyembry@kimberlys-MacBook-Air.local>
2016-12-13 10:05:25 -0800

    Week2 checkpoint challenge

--- soda_machine.rb
+++ soda_machine.rb
@@ -1,3 +1,5 @@
+require_relative "soda"
+
 class SodaMachine
   attr_reader :sodas, :cash
 
@@ -6,13 +8,45 @@ class SodaMachine
     @cash = args[:cash]
   end
 
+  # return the number of sodas it contains
   def current_inventory_count
+    @sodas.length
   end
 
+  # find single soda with a given brand
   def find_soda(soda_brand)
+    @sodas.each do |soda|
+      if soda.brand == soda_brand
+        return soda
+      else
+        nil
+      end
+    end
   end
 
+  # sell a soda with a given brand
   def sell(soda_brand)
+    if @sodas.include(soda_brand) then false
+      nil
+    elsif
+      @sodas.each do |soda|
+        if soda.brand == soda_brand
+          @cash += soda.price
+          @sodas.delete(soda)
+          @cash
+        else
+          nil
+        end
+      end
+    else
+      nil
+    end
   end
 
 end
+
+machine = SodaMachine.new
+p machine.current_inventory_count
+p machine.find_soda("Mountain Dew")
+p machine.find_soda("Surge")
+p machine.sell("Surge")
