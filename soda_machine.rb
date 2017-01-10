3fa8b072035ff62b92e30813036283b6b41d5984
bacevedo01 <bacevedo@gmail.com>
2017-01-10 10:11:19 -0800

    Add method for current inventory.

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,12 +7,15 @@ class SodaMachine
   end
 
   def current_inventory_count
+    current_inventory_count = @sodas.length
   end
 
   def find_soda(soda_brand)
+    @sodas.select { |x| x[:brand] == 'soda_brand' }.map { |u| u[:brand] }
   end
 
   def sell(soda_brand)
+
   end
 
 end
