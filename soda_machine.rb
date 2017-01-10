5ddba38b9e285235b6e30fe9e0a9b12c67018cba
Samantha Gomez <sgomez_21@yahoo.com>
2017-01-10 09:32:12 -0800

    worked through 2 methods so far

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,9 +7,11 @@ class SodaMachine
   end
 
   def current_inventory_count
+    @sodas.length
   end
 
   def find_soda(soda_brand)
+    @sodas.find {|soda| soda.brand == soda_brand}
   end
 
   def sell(soda_brand)
