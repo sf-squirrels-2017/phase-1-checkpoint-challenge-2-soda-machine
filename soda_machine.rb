323d5e0e19d82456e048b9fad12a2a887aa527b8
shaunyee <email": null>
2017-01-10 10:13:47 -0800

    checkpoint challenge

--- soda_machine.rb
+++ soda_machine.rb
@@ -1,18 +1,25 @@
 class SodaMachine
   attr_reader :sodas, :cash
+  attr_accessor :sold_soda
 
   def initialize(args = {})
     @sodas = args[:sodas]
     @cash = args[:cash]
+    @sold_soda = @brand
   end
 
   def current_inventory_count
+    @sodas.count
   end
 
   def find_soda(soda_brand)
+    i = @sodas.index(soda_brand)
+    p @sodas[i]
   end
 
   def sell(soda_brand)
-  end
+@sold_soda = (soda_brand)
 
+
+  end
 end
