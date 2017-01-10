419e478582704259447695faec25d9aa94a74554
Brandon Garibaldi <bmg.oak@gmail.com>
2017-01-10 10:21:46 -0800

    implemented current inventory method

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,12 +7,21 @@ class SodaMachine
   end
 
   def current_inventory_count
-  end
-
-  def find_soda(soda_brand)
+    @sodas.length
   end
 
   def sell(soda_brand)
+    soda_brand = soda_brand.downcase.split
+    soda_brand = soda_brand.join("_")
+      if soda_brand == #instance_name - couldn't find how to id this
+        return soda_brand
+      else
+        nil
+      end
+  end
+
+  def sell(soda_brand)
+
   end
 
 end
