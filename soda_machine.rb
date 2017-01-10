d8c51e5d8696944b1e99d18cfe4ab5eafdc55f99
davis bare <email": null>
2017-01-10 10:11:25 -0800

    current intentroy method working, not done find soda or sell methods

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,12 +7,15 @@ class SodaMachine
   end
 
   def current_inventory_count
+    @sodas.length
   end
 
   def find_soda(soda_brand)
+    @sodas.index(soda_brand)
   end
 
   def sell(soda_brand)
+
   end
 
 end
