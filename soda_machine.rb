cf1b07d19376c4b455ae9591c8a166ce35951d79
Ethan Fertsch <efertsch@gmail.com>
2017-01-10 09:41:55 -0800

    write methods to count, find, and sell sodas in soda machine, to refactor last method

--- soda_machine.rb
+++ soda_machine.rb
@@ -1,5 +1,6 @@
 class SodaMachine
   attr_reader :sodas, :cash
+  attr_writer :cash
 
   def initialize(args = {})
     @sodas = args[:sodas]
@@ -7,12 +8,29 @@ class SodaMachine
   end
 
   def current_inventory_count
+    self.sodas.count
   end
 
   def find_soda(soda_brand)
+    self.sodas.find {|soda| soda.brand == soda_brand}
   end
 
+  # def sell(soda_brand)
+  #   sold_soda = find_soda(soda_brand)
+  #   sold_soda ? self.cash += sold_soda.price
+  #   # self.sodas.delete(sold_soda)
+  #   sold_soda
+  # end
+
+
+
   def sell(soda_brand)
+    sold_soda = find_soda(soda_brand)
+    if sold_soda
+      self.cash += sold_soda.price
+      sodas.delete(sold_soda)
+    end
+    sold_soda
   end
 
 end
