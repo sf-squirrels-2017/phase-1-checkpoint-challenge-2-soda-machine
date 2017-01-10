ce825eda4018cdeb9ca00dbb4a94e4ea157dcb77
George Babayan <ggbabayan@gmail.com>
2017-01-10 09:47:44 -0800

    I would rather be playing SC2.

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,12 +7,16 @@ class SodaMachine
   end
 
   def current_inventory_count
+    @sodas.size
   end
 
   def find_soda(soda_brand)
+    @sodas.find {|soda| soda.brand == soda_brand}
   end
 
   def sell(soda_brand)
+    @cash+=find_soda(soda_brand).price if find_soda(soda_brand)
+    @sodas.delete(find_soda(soda_brand))
   end
 
 end
