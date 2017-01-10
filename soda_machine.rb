5b036daeaaeaf2ff1cfaaf7c064abe68ba5e14b5
[01;31m[K"name"[m[K: "Matthew Eller", &   [01;31m[K"name"[m[K: "Brandon Manuel Garibaldi <pair+mattheweller.setthecontrols@devbootcamp.com>
2017-01-10 10:13:50 -0800

    All but one test. Damn you 'removes the sold soda from the machine'

--- soda_machine.rb
+++ soda_machine.rb
@@ -1,4 +1,6 @@
-class SodaMachine
+require_relative 'soda'
+
+class SodaMachine < Soda
   attr_reader :sodas, :cash
 
   def initialize(args = {})
@@ -7,12 +9,27 @@ class SodaMachine
   end
 
   def current_inventory_count
+    @sodas.length
   end
 
   def find_soda(soda_brand)
+    @sodas.each do |soda|
+      if soda.brand[soda_brand]
+        return soda
+      end
+    end
+    nil
   end
 
   def sell(soda_brand)
+    if find_soda(soda_brand)
+      sold_soda = find_soda(soda_brand)
+      @cash += sold_soda.price
+      # @sodas.delete.sold_soda # Can't figure out why this isn't working, and why it blows everything up
+    else
+      return find_soda(soda_brand)
+    end
+    sold_soda
   end
 
 end
