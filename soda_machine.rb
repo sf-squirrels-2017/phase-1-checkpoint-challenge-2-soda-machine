75222865318bb2aec8ab501d6f7a3754b3a34a0b
Ethan Fertsch <efertsch@gmail.com>
2017-01-10 10:08:06 -0800

    refactor sell method, make more explicit using unless and nil? for evaluation

--- soda_machine.rb
+++ soda_machine.rb
@@ -1,6 +1,6 @@
 class SodaMachine
-  attr_reader :sodas, :cash
-  attr_writer :cash
+  attr_reader :sodas
+  attr_accessor :cash
 
   def initialize(args = {})
     @sodas = args[:sodas]
@@ -15,22 +15,13 @@ class SodaMachine
     self.sodas.find {|soda| soda.brand == soda_brand}
   end
 
-  # def sell(soda_brand)
-  #   sold_soda = find_soda(soda_brand)
-  #   sold_soda ? self.cash += sold_soda.price
-  #   # self.sodas.delete(sold_soda)
-  #   sold_soda
-  # end
-
-
 
   def sell(soda_brand)
     sold_soda = find_soda(soda_brand)
-    if sold_soda
+    unless sold_soda.nil?
       self.cash += sold_soda.price
-      sodas.delete(sold_soda)
+      self.sodas.delete(sold_soda)
     end
-    sold_soda
   end
 
 end
