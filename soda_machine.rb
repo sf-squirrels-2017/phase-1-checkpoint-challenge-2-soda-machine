136483ecb4f35aebb4375258d0b52933abac378c
kimberly embry <kimberlyembry@kimberlys-MacBook-Air.local>
2017-01-10 09:45:58 -0800

    Add or remove comments

--- soda_machine.rb
+++ soda_machine.rb
@@ -8,18 +8,14 @@ class SodaMachine
     @cash = args[:cash]
   end
 
-  # return the number of sodas it contains
   def current_inventory_count
-    # @sodas.length
-    @sodas.size
+    @sodas.length # or .size
   end
 
-  # find single soda with a given brand
   def find_soda(soda_brand)
     @sodas.find {|soda| soda.brand == soda_brand}
   end
 
-  # sell a soda with a given brand
   def sell(soda_brand)
     return nil unless
       @sold_soda = find_soda(soda_brand)
@@ -27,17 +23,16 @@ class SodaMachine
       @cash += @sold_soda.price
       @sold_soda
     end
-  end
 
-  #   if @sold_soda = find_soda(soda_brand)
-  #     @sodas.delete(@sold_soda)
-  #     @cash += @sold_soda.price
-  #     return @sold_soda
-  #   else
-  #     nil
-  #   end
-  # end
-# end
+    #   if @sold_soda = find_soda(soda_brand)
+    #     @sodas.delete(@sold_soda)
+    #     @cash += @sold_soda.price
+    #     return @sold_soda
+    #   else
+    #     nil
+    #   end
+    # end
+  end
 
 # NOTES
 # Remember to look at the Rspec tests for clues
