e73a12102563332d1640dd6f7f335a3ef83f091a
Jon Bent <email": null>
2017-01-10 10:11:05 -0800

    Add logic for 3 functions

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,12 +7,25 @@ class SodaMachine
   end
 
   def current_inventory_count
+    return @sodas.length
   end
 
   def find_soda(soda_brand)
+    soda = soda_brand.downcase.split(' ').join('_')
+    sodas.find {|x| x.brand == soda}
   end
 
   def sell(soda_brand)
+    changed_name = soda_brand.downcase.split(' ').join('_')
+    length = sodas.length
+    sodas.each do |i|
+      if sodas.length < length
+        break
+      else
+        sodas.delete(i.)
+    end
   end
 
 end
+
+
