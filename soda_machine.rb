fbd799f390214bf0019241855c05cb1f4c1e6bb7
Michael <email": null>
2017-01-10 15:08:09 -0800

    found number of sodas, but could not pull out individual sodas from array

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,9 +7,17 @@ class SodaMachine
   end
 
   def current_inventory_count
+    @sodas.length
   end
 
   def find_soda(soda_brand)
+    # @sodas.select{|key, hash| hash["brand"] == soda_brand }
+    # @sodas.key({"brand" => soda_brand})
+    # @sodas.select {|e| e == soda_brand}
+    # @sodas.detect {|e| e == soda_brand}
+    @sodas.select {|y| y == soda_brand}
+    # p "the value is #{soda_brand}"
+
   end
 
   def sell(soda_brand)
