84e624bd498291bea958fe4aed878f4a6c8296f4
Alex Gonzalez <alex.gm62288@gmail.com>
2017-01-10 15:17:49 -0800

    initial commit

--- soda_machine.rb
+++ soda_machine.rb
@@ -1,5 +1,5 @@
 class SodaMachine
-  attr_reader :sodas, :cash
+  attr_accessor :sodas, :cash, :sold_soda
 
   def initialize(args = {})
     @sodas = args[:sodas]
@@ -7,12 +7,26 @@ class SodaMachine
   end
 
   def current_inventory_count
+    @sodas.length
   end
 
   def find_soda(soda_brand)
+    @sodas.each do |soda|
+      if soda.brand == soda_brand
+        return soda
+      end
+    end
+    nil
   end
 
-  def sell(soda_brand)
+    def sell(soda_brand)
+      @sodas.each do |soda|
+        if soda.brand == soda_brand
+           @cash += soda.price
+           @sodas.delete(soda)
+           return soda
+        end
+      end
+      nil
+    end
   end
-
-end
