66150445aae0bdd6a86a3a97f2224216e755fdb2
Aaron Calander <email": null>
2017-01-10 10:15:56 -0800

    Add methods and passed 4 tests

--- soda_machine.rb
+++ soda_machine.rb
@@ -1,5 +1,5 @@
 class SodaMachine
-  attr_reader :sodas, :cash
+  attr_reader :sodas, :cash, :inventory
 
   def initialize(args = {})
     @sodas = args[:sodas]
@@ -7,12 +7,26 @@ class SodaMachine
   end
 
   def current_inventory_count
+    return @sodas.size
   end
 
   def find_soda(soda_brand)
+    @sodas.each do |soda|
+      if soda.brand == soda_brand
+        return soda
+      else
+        return nil
+      end
+    end
   end
+# Got caught up with the previous method and ran out of time!
 
   def sell(soda_brand)
+    @sodas.each do |soda|
+      if soda.brand == soda_brand
+        return soda
+      end
+    end
   end
 
 end
