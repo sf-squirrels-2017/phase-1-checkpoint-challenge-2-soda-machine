9e4d83e05eda8bec84edcbea8662856b60824924
kimberly embry <kimberlyembry@kimberlys-MacBook-Air.local>
2017-01-10 09:42:02 -0800

    Refactor initial #sell

--- soda_machine.rb
+++ soda_machine.rb
@@ -21,21 +21,23 @@ class SodaMachine
 
   # sell a soda with a given brand
   def sell(soda_brand)
-    # return nil unless
-    #   @sold_soda = find_soda(soda_brand)
-    #   @sodas.delete(@sold_soda)
-    #   @cash += @sold_soda.price
-    # end
-    # @sold_soda
-    if @sold_soda = find_soda(soda_brand)
+    return nil unless
+      @sold_soda = find_soda(soda_brand)
       @sodas.delete(@sold_soda)
       @cash += @sold_soda.price
-      return @sold_soda
-    else
-      nil
+      @sold_soda
     end
-  end 
   end
+
+  #   if @sold_soda = find_soda(soda_brand)
+  #     @sodas.delete(@sold_soda)
+  #     @cash += @sold_soda.price
+  #     return @sold_soda
+  #   else
+  #     nil
+  #   end
+  # end
+# end
 
 # NOTES
 # Remember to look at the Rspec tests for clues
