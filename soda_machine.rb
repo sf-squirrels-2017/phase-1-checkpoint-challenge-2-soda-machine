81568957cc927374c5414a2263ea62dde17677e8
Terrence Wu <tonycwu888@gmail.com>
2017-01-10 10:12:38 -0800

    was able to only turn 1 red to green

--- soda_machine.rb
+++ soda_machine.rb
@@ -4,12 +4,18 @@ class SodaMachine
   def initialize(args = {})
     @sodas = args[:sodas]
     @cash = args[:cash]
+    
   end
 
   def current_inventory_count
+    @sodas.length
+  
   end
 
   def find_soda(soda_brand)
+    @sodas.each do |soda, price|
+      if soda.fetch()
+    end
   end
 
   def sell(soda_brand)
