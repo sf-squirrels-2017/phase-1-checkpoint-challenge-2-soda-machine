70c028681ca669c5754f7c1e26f32258b0c34020
Jon Bent <email": null>
2017-01-15 13:49:32 -0800

    added logic to current_inventory_count, find_soda, sell methods

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,12 +7,20 @@ class SodaMachine
   end
 
   def current_inventory_count
+    @sodas.length
   end
 
   def find_soda(soda_brand)
+    @sodas.find {|each_soda| each_soda.brand == soda_brand}
   end
 
   def sell(soda_brand)
+    if find_soda(soda_brand)
+      @cash += find_soda(soda_brand).price
+      @sodas.delete(find_soda(soda_brand))
+    else
+      nil
+    end
   end
 
 end
