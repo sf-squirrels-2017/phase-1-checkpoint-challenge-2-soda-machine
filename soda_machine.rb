1a27d7bbddc9dff0d6de5d8e6888cfbff8343973
Alex Gonzalez <alex.gm62288@gmail.com>
2017-01-16 12:33:00 -0800

    initial commit

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,12 +7,26 @@ class SodaMachine
   end
 
   def current_inventory_count
+    @sodas.length
   end
 
   def find_soda(soda_brand)
+    @sodas.each do |soda|
+      if soda.brand == soda_brand
+        return soda
+      end
+    end
+      nil
   end
 
   def sell(soda_brand)
+    soda = find_soda(soda_brand)
+    if soda != nil
+      @cash += soda.price
+      @sodas.delete(soda)
+      return soda
+    else
+      nil
+    end
   end
-
 end
