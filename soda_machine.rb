029355444a286609d2da50f206f0a99b724d6892
bacevedo01 <bacevedo@gmail.com>
2017-01-16 11:18:15 -0800

    Add method for current inventory.

--- soda_machine.rb
+++ soda_machine.rb
@@ -1,4 +1,15 @@
 class SodaMachine
-  
+  attr_reader :sodas, :cash, :sold_soda
+
+  def initialize(args = {})
+    @sodas = args[:sodas]
+    @cash = args[:cash]
+    @sold_soda = []
+  end
+
+  def current_inventory_count
+    current_inventory_count = @sodas.count
+  end
+
 
 end
