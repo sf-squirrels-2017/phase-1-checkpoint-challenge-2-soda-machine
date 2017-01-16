b66461a1c7b3e073c702680747d074b7efd34ae4
bacevedo01 <bacevedo@gmail.com>
2017-01-16 11:09:28 -0800

    Cleared file to redo challenge.

--- soda_machine.rb
+++ soda_machine.rb
@@ -1,21 +1,4 @@
 class SodaMachine
-  attr_reader :sodas, :cash
-
-  def initialize(args = {})
-    @sodas = args[:sodas]
-    @cash = args[:cash]
-  end
-
-  def current_inventory_count
-    current_inventory_count = @sodas.length
-  end
-
-  def find_soda(soda_brand)
-    @sodas.select { |x| x[:brand] == 'soda_brand' }
-  end
-
-  def sell(soda_brand)
-
-  end
+  
 
 end
