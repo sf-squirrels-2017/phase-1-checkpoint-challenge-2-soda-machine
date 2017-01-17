8d0bf3026e45130828d02caa21be55233da0befa
Michael <email": null>
2017-01-16 20:08:42 -0800

    'find_soda' i FOUND an instance of Pepsi. in 'sell' i pulled out instance of soda, removed from array of soda instances, added price to change drawer, and distributed a soda

--- soda_machine.rb
+++ soda_machine.rb
@@ -11,16 +11,24 @@ class SodaMachine
   end
 
   def find_soda(soda_brand)
-    # @sodas.select{|key, hash| hash["brand"] == soda_brand }
-    # @sodas.key({"brand" => soda_brand})
-    # @sodas.select {|e| e == soda_brand}
-    # @sodas.detect {|e| e == soda_brand}
-    @sodas.find {|soda| soda.brand == soda_brand}
-    # p "the value is #{soda_brand}"
+    @sodas.find {|s| s.brand == soda_brand}
+    # goes through each soda object, pulls out first soda name matching soda_brand
 
   end
 
+  # Distributes a soda check
+  # Decreases inventory check
+  # add to change drawer check
   def sell(soda_brand)
+    unique_soda = find_soda(soda_brand)
+    if unique_soda != nil
+      @cash = @cash + unique_soda.price
+      # since on soda object is 'Surge', returns 'nil'
+      @sodas.delete(unique_soda)
+    end
   end
 
+  def cash
+    @cash * 1.00       #value from hash - soda_name = price hash
+  end
 end
