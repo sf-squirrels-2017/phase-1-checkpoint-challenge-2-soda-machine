e636723f37ba65c31c03d275fe5ce86e04197ca9
denise duffy <denise_duffy@denises-MacBook-Pro.local>
2017-01-31 19:45:04 -0800

    Add methods to soda_machine.rb

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,12 +7,26 @@ class SodaMachine
   end
 
   def current_inventory_count
+    sodas.count
   end
 
   def find_soda(soda_brand)
+    sodas.find do |soda|
+      soda.brand == soda_brand
+    end
+
   end
 
   def sell(soda_brand)
+    if find_soda(soda_brand)
+      #p " * This is the soda * " * 4
+      soda_object = find_soda(soda_brand)
+      @cash += soda_object.price
+      sodas.delete(soda_object)
+      soda_object
+    else
+      return nil
+    end
   end
 
 end
